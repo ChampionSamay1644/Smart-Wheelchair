107e5bb95ccf299bc7590e2ec9dc0a6ad8e11b14
AiLover <nishalpoojary71@gmail.com>
2025-10-30 20:15:21 +0530

    Add Firebase core directory with options

---
+++ smart_wheelchair_app/lib/core/firebase/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+// TODO: Replace with your Firebase configuration
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform => const FirebaseOptions(
+    apiKey: 'YOUR-API-KEY',
+    appId: 'YOUR-APP-ID',
+    messagingSenderId: 'YOUR-SENDER-ID',
+    projectId: 'YOUR-PROJECT-ID',
+    authDomain: 'YOUR-AUTH-DOMAIN',
+    storageBucket: 'YOUR-STORAGE-BUCKET',
+  );
+}
